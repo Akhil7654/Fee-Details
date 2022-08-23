c93a2c30fcec55eb71968f614f7de88ea46d70fc
Akhil7654 <msdcr88@gmail.com>
2022-08-23 22:30:29 +0530

    SIZE ADJUST AND GRID CARD ADD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedetailComponent } from './feedetail/feedetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedetailComponent
   ],
   imports: [
     BrowserModule,
